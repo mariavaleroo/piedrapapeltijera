5407166276238a610ae3231c0301f6c4abd2cf1a
[mariavaleroo_] <[mariagarcia.daw@ciudadescolarfp.es]>
2022-02-03 13:34:46 +0100

    programa

---
+++ ppt/ppt.php
@@ -0,0 +1,181 @@
+<html>
+<head>
+    <style>
+        body{
+            background:url(images.jpg);
+            background-size:cover;
+            background-repeat:no-repeat;
+        }
+        input[name="piedra"]{
+            background: url(piedra2.png);
+            cursor: pointer;
+            padding: 100px;
+            background-size: cover;
+            background-repeat: no-repeat;
+            border:none
+        }
+        input[name="papel"]{
+            background: url(papel2.png);
+            cursor: pointer;
+            padding: 100px;
+            background-size: cover;
+            background-repeat: no-repeat;
+            border:none
+
+        }
+        input[name="tijera"]{
+            background: url(tijera2.png);
+            cursor: pointer;
+            padding: 100px;
+            background-size: cover;
+            background-repeat: no-repeat;
+            border:none
+
+        }
+        img{
+            width:150px;
+            background: #ffffff70;
+        }
+        .form{
+            display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    align-content: center;
+    background: #ffffff70;
+        }
+        h1{
+            color: white;
+    text-align: center;
+    font-size: 40px;
+        }
+        p{
+            color: white;
+    text-align: center;
+    font-size: 40px;
+    border: 1px solid white;
+    text-align: center;
+    background: cadetblue;
+    padding: 20px;
+    position: absolute;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    margin: 0px;
+
+        }
+        .todo{
+            display: flex;
+    justify-content: space-around;
+    align-items: center;
+        }
+    </style>
+</head>
+</html>
+<?php
+session_start();
+if(isset($_POST["inicio"])){
+    $_SESSION["nombre"]=$_POST["nombre"];
+};
+echo "<h1>".$_SESSION["nombre"]." VS Máquina</h1>";
+echo "<div class='todo'>";
+
+echo "<div class='form'>
+<form action='' class='piedra' method='post'>
+<input type='submit' name='piedra' value=''>
+</form>
+
+<form action='' class='papel' method='post'>
+<input type='submit' name='papel' value=''>
+</form>
+
+<form action='' class='tijera' method='post'>
+<input type='submit' name='tijera' value=''>
+</form></div>
+
+";
+$_SESSION["resultado"]=mt_rand(1,3);
+//1=piedra
+//2=papel
+//3=tijera
+if(isset($_POST["piedra"])){
+
+        echo "<div class='maquina'>";
+        if($_SESSION["resultado"]==1){
+            echo "<img class='imagen' src='piedra2.png'>";
+        }elseif($_SESSION["resultado"]==2){
+            echo "<img class='imagen' src='papel2.png'>";
+
+        }else{
+            echo "<img class='imagen' src='tijera2.png'>";
+
+        }
+
+    if($_SESSION["resultado"]==1){
+        echo "<p>Has empatado</p>";
+    }elseif($_SESSION["resultado"]==2){
+        echo "<p>Has perdido</p>";
+    }else{
+        echo "<p>Has ganado</p>";
+    }
+    echo "</div>";
+
+    
+}
+
+
+if(isset($_POST["papel"])){
+    echo "<div class='maquina'>";
+
+    if($_SESSION["resultado"]==1){
+        echo "<img class='imagen' src='piedra2.png'>";
+    }elseif($_SESSION["resultado"]==2){
+        echo "<img class='imagen' src='papel2.png'>";
+
+    }else{
+        echo "<img class='imagen' src='tijera2.png'>";
+
+    }
+
+
+    if($_SESSION["resultado"]==1){
+        echo "<p>Has ganado</p>";
+    }elseif($_SESSION["resultado"]==2){
+        echo "<p>Has empatado</p>";
+    }else{
+        echo "<p>Has perdido</p>";
+    }
+    echo "</div>";
+
+}
+
+
+if(isset($_POST["tijera"])){
+    echo "<div class='maquina'>";
+
+    if($_SESSION["resultado"]==1){
+        echo "<img class='imagen' src='piedra2.png'>";
+    }elseif($_SESSION["resultado"]==2){
+        echo "<img class='imagen' src='papel2.png'>";
+
+    }else{
+        echo "<img class='imagen' src='tijera2.png'>";
+
+    }
+
+    if($_SESSION["resultado"]==1){
+        echo "<p>Has perdido</p>";
+    }elseif($_SESSION["resultado"]==2){
+        echo "<p>Has ganado</p>";
+    }else{
+        echo "<p>Has empatado</p>";
+    }
+    echo "</div>";
+
+}
+
+echo "</div>";
+
+
+?>
+
---
+++ ppt/ppt_inicio.php
@@ -0,0 +1,64 @@
+
+<html>
+<head>
+    <style>
+        body{
+            background:url(images.jpg);
+            background-size:cover;
+            background-repeat:no-repeat;
+            display: flex;
+    align-items: center;
+    justify-content: center;
+    align-content: center;
+        }
+
+        .inicio{
+            padding: 50px;
+    background: cadetblue;
+        }
+        h1{
+            color: white;
+    text-align: center;
+    font-size: 40px;
+        }
+        input[type="text"]{
+            text-align: center;
+    border-radius: 18px;
+    padding: 10px;
+    border: 1px solid darkblue;
+        }
+        input[type="submit"]{
+            text-align: center;
+    border-radius: 18px;
+    padding: 10px;
+    border: 1px solid darkblue;
+    background: transparent;
+        }
+ form{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    align-content: center;
+ }
+    </style>
+</head>
+</html>
+
+<?php
+
+echo "
+<div class='inicio'>
+<h1>Introduce tu nombre</h1>
+<form action='ppt.php' method='post'>
+
+
+<input type='text' required name='nombre' placeholder='nombre'>
+
+<input type='submit' name='inicio' value='comenzar'>
+</form>
+
+</div>
+
+";
+
+?>
